3d8194df57d970f155405c91d1c486840441f8e7
Sanyu Kimuli <sanyukimuli@gmail.com>
2025-06-07 13:09:49 +0100

    math-object

---
+++ math-object.js
@@ -0,0 +1,52 @@
+// These are math properties (constants)
+
+Math.E ;
+console.log(Math.E);
+
+Math.PI ;
+console.log(Math.PI) ;
+
+Math.LN10 ;
+console.log(Math.LN10) ;
+
+Math.SQRT1_2 ;
+console.log(Math.SQRT1_2);
+
+
+// These are Math Methods
+
+Math.round(4.6); // this is rounded to the nearest integer
+Math.round(4.3);
+
+Math.ceil(3.2); // this is rounded up to the nearest integer
+
+Math.floor(3.9); // this is rounded down to the nearest integer
+
+Math.trunc(3.9384); // truncating is cutting anything after the decimal
+
+console.log(Math.round(4.6) , Math.round(4.3) , Math.ceil(3.2), Math.floor(3.9) , Math.trunc(3.9384));
+
+Math.sign(-6.3); // -1 means it's negative
+Math.sign(0); // 0 means it's null
+Math.sign(929908); // 1 means it's positive
+
+console.log(Math.sign(-6.3),Math.sign(0), Math.sign(929908) );
+
+Math.pow(6,3); // returns 6 to the power of 3 
+Math.sqrt(100); // square root of 100
+Math.abs(-4.7); // this makes a negative positive
+Math.abs(4.7); // pos remains pos 
+
+console.log(Math.pow(6,3), Math.sqrt(100), Math.abs(-4.7), Math.abs(4.7));
+
+Math.min(4, 66, 9, 2020, 5000); // the smallest number is returned
+Math.max(4, 66, 9, 2020, 5000); // the biggest number is returned
+
+console.log(Math.min(4, 66, 9, 2020, 5000), Math.max(4, 66, 9, 2020, 5000));
+
+Math.random () // any random number between 0 and 0.9
+Math.random () 
+
+console.log(Math.random(),Math.random () );
+
+
